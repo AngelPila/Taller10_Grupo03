d034ce07377eb9ce1090b1379a1adbad6d1697f1
ivancede <>
2025-07-30 13:37:34 -0500

    BASE
    BASE DE TALLER

--- src/main/java/ec/edu/ec/Operations.java
+++ src/main/java/ec/edu/ec/Operations.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package calculatorsmp;
+package ec.edu.ec;
+
 import java.util.Stack;
-/**
- *
- * @author Djurado
- */
+
 public class Operations {
 
     private static String[] op = { "+", "-", "*", "/" };// Operation set
